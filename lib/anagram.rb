bba9b70fbd686b71783279fcc0ec54781c19c371
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-08-02 04:49:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,13 +9,13 @@ class Anagrams
   end
 
   def match(anagram_array)
-    # #match is going to take our word arguement and find any possible 
+    # #match is going to take our word arguement and find any possible
     # anagram matches in our anagram_array arguement
 
-    # our word and possible anagrams need 
+    # our word and possible anagrams need
     # to be split up and sorted to be compared
 
     binding.pry
-    
+
   end
 end
